8f00b5e3f3fb7dced62745c47647692c024f1ec3
OxyTom <oxytom8@gmail.com>
2020-12-30 23:11:24 +0100

    minor corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from 'react-router-dom';
 
 import Home from "./views/home";
 
-function App() {
+const App = () => {
   return (
     <HashRouter basename='/'>
       <Route exact path="/" component={Home} />
