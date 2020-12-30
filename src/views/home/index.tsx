f5a7d4e81d12493d8d8b04bd0270e7c222082741
OxyTom <oxytom8@gmail.com>
2020-12-30 23:18:19 +0100

    Removing maximum of css

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,24 +1,71 @@
+import styled from '@emotion/styled';
+
 import logo from "../../logo.svg";
-import "../../App.css";
+
+// #region Styles
+
+const App = styled.div`
+    text-align: center;
+`
+
+const Header = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`
+
+const Logo = styled.img`
+    height: 40vmin;
+    pointer-events: none;
+
+    @media (prefers-reduced-motion: no-preference) {
+        & {
+          animation: App-logo-spin infinite 20s linear;
+        }
+    }
+
+    @keyframes App-logo-spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+`
+
+const Code = styled.code`
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+`
+
+const Link = styled.a`
+    color: #61dafb;
+`
+
+// #endregion
 
 const Home = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+        <App>
+            <Header>
+                <Logo src={logo} alt="logo" />
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                    Edit <Code>src/App.tsx</Code> and save to reload.
                 </p>
-                <a
-                    className="App-link"
+                <Link
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     Learn React
-                </a>
-            </header>
-        </div>
+                </Link>
+            </Header>
+        </App>
     );
 }
 
