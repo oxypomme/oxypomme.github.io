fc1b52ec35153608ab25f2bfdcdd1b3df5b35ace
OxyTom <oxytom8@gmail.com>
2020-12-31 13:02:46 +0100

    added some usefull components

--- src/components/styledComponents.ts
+++ src/components/styledComponents.ts
@@ -5,3 +5,37 @@ export const Separator = styled.div`
     width: 45%;
     margin: 15px auto 15px auto;
 `;
+
+export const HiddenList = styled.ul`
+    list-style-type: none;
+    padding: 0;
+
+    & ul {
+        padding: 0 20px;
+    }
+
+    & h4 {
+        display: inline;
+    }
+`;
+
+export const Card = styled.div`
+    text-align: left;
+    width: 700px;
+    margin: 20px;
+    padding: 10px 20px;
+    background-color: var(--accent2-transparent);
+    border-radius: 5px;
+    transition: background-color .25s;
+
+    &:hover{
+        background-color: var(--accent2-dark-transparent);
+    }
+`;
+
+export const FlexContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: flex-start;
+`;
